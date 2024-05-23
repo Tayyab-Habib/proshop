2b3da067ebc354f0ceeda5973da5d1ccf12ee3e8
Tayyab-Habib <tayyabhabib34567@gmail.com>
2024-05-23 21:30:18 +0500

    Backend Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 // const express =require('express')
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
 dotenv.config()
 import express from 'express'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 
@@ -12,11 +14,22 @@ const port = process.env.PORT
 
 connectDB() //Connect to MongoDB
 const app = express()
+
+
+//Body parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+
+// cookie parser middleware
+app.use(cookieParser())
+
+
 app.get('/',(req,res)=>{
     res.send("API is running...")
 })
 
 app.use('/api/products',productRoutes)
+app.use('/api/users',userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
